72c77de01813f4f26647e3c5d1660f3122cba37e
25vudinhvinh <25vudinhvinh@gmail.com>
2025-06-22 00:26:57 +0700

    [update] api product limit 8

--- src/route/productsRoute.js
+++ src/route/productsRoute.js
@@ -5,7 +5,7 @@ const Products = require("../controllers/productsController");
 router.get("/hot", Products.getHotProducts);
 router.get("/adidas-limit", Products.getAdidas);
 router.get("/nike-limit", Products.getNike);
-router.get("/mlblimit", Products.getMLB);
+router.get("/mlb-limit", Products.getMLB);
 router.get("/newbalance-limit", Products.getNEWBALANCE);
 router.post("/detail", Products.getDetail);
 module.exports = router;
